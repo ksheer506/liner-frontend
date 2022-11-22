8606bd35ab1eabd930ad7e28c078cf44b669da60
ksheer506 <ksheer506@naver.com>
2022-11-22 14:53:55 +0900

    feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { SharedLayout } from "components";
 import { Main, Search } from "pages";
 import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/search" element={<Search />} />
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Main />} />
+        <Route path="/search/*" element={<Search />} />
+      </Route>
     </Routes>
   );
 }
