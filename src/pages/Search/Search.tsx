2053bc63eb4ef461337d39c2c1743a967073ac46
ksheer506 <ksheer506@naver.com>
2022-11-22 17:32:42 +0900

    #2 style: 전체 페이지 스크롤 제거

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,18 +1,32 @@
+import { SEARCH_PAGE_HEADER_HEIGHT } from "assets";
 import { ContentItem, SearchPageHeader } from "components";
 import styled from "styled-components";
 
 export const Search = () => {
   return (
-    <>
+    <Box>
       <SearchPageHeader />
-      <ResultList />
-      <ContentItem
-        title="The Pitfalls of Using Google Ngram to Study Language | The Pitfalls of Using Google Ngram to Study Language Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-        url="https://www.naver.com"
-        isBookmarked
-      />
-    </>
+      <ResultList>
+        {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((e, i) => (
+          <ContentItem
+            title="The Pitfalls of Using Google Ngram to Study Language | The Pitfalls of Using Google Ngram to Study Language Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
+            url="https://www.naver.com"
+            isBookmarked
+            key={i}
+          />
+        ))}
+      </ResultList>
+    </Box>
   );
 };
 
-const ResultList = styled.ul``;
+const Box = styled.div`
+  height: 100vh;
+  overflow: hidden;
+`;
+
+const ResultList = styled.ul`
+  margin-top: 0;
+  overflow: scroll;
+  height: calc(100% - ${SEARCH_PAGE_HEADER_HEIGHT});
+`;
