f7e48c7fad658d411d57a10590c27c4b6fc7731c
ksheer506 <ksheer506@naver.com>
2022-11-23 18:08:37 +0900

    #2 feat: removeFromBookmark API 추가

--- src/apis/bookmark.ts
+++ src/apis/bookmark.ts
@@ -1,9 +1,17 @@
 import { axiosInstance } from "utils";
 
 export const addToBookmark = async (itemId: string) => {
-const { data } = await axiosInstance.post(`/collection/document/${itemId}`);
+  const { data, status } = await axiosInstance.post(
+    `/collection/document/${itemId}`
+  );
+
+  return { data, status };
 };
 
 export const removeFromBookmark = async (itemId: string) => {
+  const { data, status } = await axiosInstance.delete(
+    `/collection/document/${itemId}`
+  );
 
+  return { data, status };
 };
