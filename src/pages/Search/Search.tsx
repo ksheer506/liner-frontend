f508ccd95bf261bda14d9e39abcc07157c3487f2
ksheer506 <ksheer506@naver.com>
2022-11-23 18:33:57 +0900

    #2 refactor: 검색 페이지 컴포넌트 분리

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,32 +1,21 @@
-import { SEARCH_PAGE_HEADER_HEIGHT } from "assets";
-import { ContentItem, SearchPageHeader } from "components";
+import {
+  DocumentHeader,
+  DocumentList,
+} from "components";
 import styled from "styled-components";
 
 export const Search = () => {
   return (
     <Box>
-      <SearchPageHeader />
-      <ResultList>
-        {[0, 0, 0, 0, 0, 0, 0, 0, 0, 0].map((e, i) => (
-          <ContentItem
-            title="The Pitfalls of Using Google Ngram to Study Language | The Pitfalls of Using Google Ngram to Study Language Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum"
-            url="https://www.naver.com"
-            isBookmarked
-            key={i}
-          />
-        ))}
-      </ResultList>
+      <DocumentHeader />
+      <DocumentList />
     </Box>
   );
 };
 
 const Box = styled.div`
-  height: 100vh;
-  overflow: hidden;
+  /* height: 100vh; */
+  /* overflow: hidden; */
 `;
 
-const ResultList = styled.ul`
-  margin-top: 0;
-  overflow: scroll;
-  height: calc(100% - ${SEARCH_PAGE_HEADER_HEIGHT});
-`;
+
