36d8447bf907676732911118b47b483e1f3aa8f7
ksheer506 <ksheer506@naver.com>
2022-11-24 17:18:36 +0900

    #3 feat: 검색 결과 중복 제거

--- src/components/Documents/DocumentList.tsx
+++ src/components/Documents/DocumentList.tsx
@@ -1,21 +1,34 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { searchDocuments } from "apis";
-import { SEARCH_PAGE_HEADER_HEIGHT } from "assets";
+import { colors, SEARCH_PAGE_HEADER_HEIGHT } from "assets";
+import { useModal } from "components/Modal";
+import { Error } from "components/Modal/ModalContent/Error";
 import { ContentItemSkeleton } from "components/Skeleton/ContentItemSkeleton";
-import { useInfiniteScroll } from "hooks";
+import { SEARCH_PARAM } from "constant";
+import { useGetQueryParam, useInfiniteScroll } from "hooks";
 import { useCallback, useRef } from "react";
 import { useInfiniteQuery } from "react-query";
-import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
-import { DocumentItemType } from "types";
+import { DocumentItemType, Identified } from "types";
 import { DocumentItem } from "./DocumentItem";
 
-const filterDuplicatedItems = <T,>(data: T[] = []) => {
-  return [...new Set(data)];
+const filterDuplicatedItems = <T extends Identified>(data: T[] = []) => {
+  const idMap = new Map(data.map(({ id }) => [id, 0]));
+
+  return data.filter(({ id }) => {
+    const firstAppearance = idMap.get(id) === 0;
+
+    if (firstAppearance) {
+      idMap.set(id, 1);
+
+      return true;
+    }
+    return false;
+  });
 };
 
 const matchDataToStatus = <T,>(
-  data: T[] = [],
+  data: (T | number)[] = [],
   isFetching: boolean,
   itemsPerPage: number
 ) => {
@@ -23,23 +36,22 @@ const matchDataToStatus = <T,>(
   const dummyArr = Array(itemsPerPage)
     .fill(0)
     .map((_, i) => data.length + i);
-  const result: (T | number)[] = data;
 
-  if (!isFetching) return result;
+  if (!isFetching) return data;
 
   // 1. 컴포넌트가 마운트될 때(result === undefined), 2. API 응답을 기다릴 때, 로딩중 Skeleton을 덧붙임
-  return result.concat(dummyArr);
+  return data.concat(dummyArr);
 };
 
-const mapContentItemsWithSkeleton = (
-  data: (DocumentItemType | number)[] | undefined,
+const mapItemsWithSkeleton = (
+  data: DocumentItemType[] | undefined,
   isFetching: boolean,
   itemsPerPage: number
 ) => {
-  const dataWithDummy = matchDataToStatus(data, isFetching, itemsPerPage);
-  const filtered = filterDuplicatedItems(dataWithDummy);
+  const filtered = filterDuplicatedItems(data);
+  const dataWithDummy = matchDataToStatus(filtered, isFetching, itemsPerPage);
 
-  return filtered.map((e) => {
+  return dataWithDummy.map((e) => {
     if (typeof e !== "number") {
       const { title, url, imageUrl, faviconUrl, isSaved, id } = e;
 
@@ -62,59 +74,93 @@ const mapContentItemsWithSkeleton = (
 
 export const DocumentList = () => {
   const size = useRef(15);
-  const [searchParams] = useSearchParams();
-  const keyword = searchParams.get("keyword") || "";
+  const keyword = useGetQueryParam(SEARCH_PARAM);
+  const { openModal } = useModal();
 
-  const { data, isFetching, fetchNextPage } = useInfiniteQuery(
-    ["documents", keyword],
-    ({ pageParam = 0 }) =>
-      searchDocuments({
-        keyword,
-        size: size.current,
-        from: pageParam,
-      }),
-    {
-      getNextPageParam: (data, allPages) => {
-        const nextFrom = allPages.length * size.current;
+  const { data, fetchNextPage, isFetching, isError, hasNextPage } =
+    useInfiniteQuery(
+      ["documents", keyword],
+      ({ pageParam = 0 }) =>
+        searchDocuments({
+          keyword,
+          size: size.current,
+          from: pageParam,
+        }),
+      {
+        getNextPageParam: ({ isLast }, allPages) => {
+          const nextFrom = allPages.length * size.current;
 
-        if (data.isLast) {
-          return undefined;
-        }
+          if (isLast) {
+            return undefined;
+          }
 
-        return nextFrom;
-      },
-      select: (data) => {
-        const transformedPages = data.pages
-          .map(({ documents }) => documents)
-          .flat();
+          return nextFrom;
+        },
+        onError: () => {
+          openModal(<Error />);
+        },
+        select: (data) => {
+          const transformedPages = data.pages
+            .map(({ documents }) => documents)
+            .flat();
 
-        return { ...data, pages: transformedPages };
-      },
-      staleTime: 1 * 60 * 1000,
-    }
-  );
+          return { ...data, pages: transformedPages };
+        },
+        staleTime: Infinity,
+        cacheTime: 0,
+        retry: 2,
+      }
+    );
   const onIntersecting = useCallback(() => {
+    if (!hasNextPage) return;
+
     fetchNextPage();
-  }, []);
+  }, [hasNextPage]);
   const { bottomRef } = useInfiniteScroll({ onIntersecting });
 
   const { pages } = data || {};
+  /* console.log(pages, isError); */
 
   return (
-    <>
+    <section>
       <ResultList>
-        <>
-          {mapContentItemsWithSkeleton(pages, isFetching, size.current)}
-          {!isFetching && !pages?.length && <div>검색 결과가 없습니다.</div>}
-        </>
+        {mapItemsWithSkeleton(pages, isFetching, size.current)}
       </ResultList>
+      {!hasNextPage && !!pages?.length && (
+        <DeadEnd>더이상 결과가 없습니다.</DeadEnd>
+      )}
+      {!isError && !pages?.length && <NoResult>검색 결과가 없습니다.</NoResult>}
       <div ref={bottomRef} />
-    </>
+    </section>
   );
 };
 
 const ResultList = styled.ul`
-  margin-top: 0;
+  margin: 0;
   overflow: scroll;
-  /* height: calc(100% - ${SEARCH_PAGE_HEADER_HEIGHT}); */
+  padding-top: calc(${SEARCH_PAGE_HEADER_HEIGHT} + 10px);
+`;
+
+const DeadEnd = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  margin-bottom: 10px;
+
+  color: ${colors("gray35")};
+  font-weight: 700;
+  font-size: 14px;
+`;
+
+const NoResult = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(95vh - ${SEARCH_PAGE_HEADER_HEIGHT});
+
+  color: ${colors("gray35")};
+  font-weight: 700;
+  font-size: 22px;
 `;
