792fb3a4ee2ecf2b0c696f32544fddda07bc97e3
ksheer506 <ksheer506@naver.com>
2022-11-24 17:18:58 +0900

    chore: Props 이름 변경

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -3,29 +3,30 @@ import { useCallback, useEffect, useRef, useState } from "react";
 interface useInfiniteScrollProps {
   onIntersecting(): void;
   coolTime?: number;
-  initializeOnMount?: boolean;
+  initiateOnMount?: boolean;
 }
 
 /**
  * @param onIntersecting 관찰하는 요소와 겹칠 때 실행할 함수
  * @param coolTime onIntersecting이 실행되는 최소 시간 간격(ms)을 설정
+ * @param initiateOnMount 컴포넌트 마운트 후 바로 무한 스크롤을 실행할지 여부 설정
  */
 export const useInfiniteScroll = <T extends Element = HTMLDivElement>({
   onIntersecting,
   coolTime = 50,
-  initializeOnMount = true,
+  initiateOnMount = true,
 }: useInfiniteScrollProps) => {
-  const [enabled, setEnabled] = useState(initializeOnMount);
+  const [enabled, setEnabled] = useState(initiateOnMount);
   const bottomRef = useRef<T>(null);
   const ioRef = useRef<IntersectionObserver>(null);
   const prevTime = useRef(0);
 
   const initialize = useCallback(() => {
-    if (initializeOnMount) return;
+    if (initiateOnMount) return;
 
     setEnabled(true);
     onIntersecting();
-  }, [initializeOnMount, onIntersecting]);
+  }, [initiateOnMount, onIntersecting]);
 
   const terminate = useCallback(() => {
     if (!ioRef.current) return;
@@ -37,7 +38,7 @@ export const useInfiniteScroll = <T extends Element = HTMLDivElement>({
   useEffect(() => {
     if (!bottomRef.current || !enabled) return;
     const options = {
-      rootMargin: "50px",
+      rootMargin: "150px",
     };
     const io = new IntersectionObserver(([{ isIntersecting, time }]) => {
       // body 하단에서 intersection 이벤트가 무한히 발생하지 않도록
