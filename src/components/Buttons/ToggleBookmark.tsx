b0ce89bf2b07be5b19788b93a58a105293c63261
ksheer506 <ksheer506@naver.com>
2022-11-25 18:16:10 +0900

    chore: 불필요한 코드 정리

--- src/components/Buttons/ToggleBookmark.tsx
+++ src/components/Buttons/ToggleBookmark.tsx
@@ -1,74 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { addToBookmark, removeFromBookmark, useBookmarkAPI } from "apis";
+import { useBookmarkAPI } from "apis";
 import { colors } from "assets";
 import { useModal } from "components/Modal";
 import { Error } from "components/Modal/ModalContent/Error";
 import { useToggleButton } from "hooks";
-import { ComponentType, ReactNode, useCallback } from "react";
-import { useMutation } from "react-query";
 import styled, { css } from "styled-components";
 import { ReactComponent as IcBookmark } from "../../assets/images/ic_bookmark.svg";
 import { ReactComponent as IcBookmarked } from "../../assets/images/ic_bookmarked.svg";
-
-/* interface Bookmark {
-  isBookmarked: boolean;
-}
-
-interface ToggleBookmarkExternalProps extends Bookmark {
-  itemId: string;
-  onClick?(itemId: string): void;
-}
-
-interface WithBookmarkAPIProps {
-  children: ReactNode;
-} */
-
-/* const withBookmarkAPI =
-  <P extends Bookmark>(Component: ComponentType<P & WithBookmarkAPIProps>) =>
-  (props: P) => {
-    const { isBookmarked } = props;
-    const { state: bookmarked, toggle: toggleBookmark } =
-      useToggleButton(isBookmarked);
-    const { openModal } = useModal();
-
-    const onMutate = useCallback(() => {
-      toggleBookmark();
-    }, []);
-    const onError = useCallback(() => {
-      openModal(<Error />);
-    }, []);
-
-    const { addMutation, removeMutation } = useBookmarkAPI({
-      onMutate,
-      onError,
-    });
-
-    if (bookmarked) {
-      return (
-        <Component
-          {...props}
-          onClick={(itemId: string) => removeMutation(itemId)}
-          isBookmarked={bookmarked}
-        >
-          <Bookmarked />
-        </Component>
-      );
-    }
-
-    return (
-      <Component
-        {...props}
-        onClick={(itemId: string) => addMutation(itemId)}
-        isBookmarked={bookmarked}
-      >
-        <UnBookmarked />
-      </Component>
-    );
-  };
-
-const ToggleBookmark = ({ itemId, onClick, children }: ToggleBookmarkProps) => {
-  return <Button onClick={() => onClick?.(itemId)}>{children}</Button>;
-}; */
 
 interface ToggleBookmarkProps {
   itemId: string;
@@ -102,8 +40,6 @@ export const ToggleBookmark = ({
     </Button>
   );
 };
-
-/* export const ToggleBookmarkWithBookmarkAPI = withBookmarkAPI(ToggleBookmark); */
 
 const Button = styled.button`
   flex: 0 0 max-content;
