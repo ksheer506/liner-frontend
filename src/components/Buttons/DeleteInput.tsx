c242ecc42999fa5d448c8b377da6e6951d2cc27c
ksheer506 <ksheer506@naver.com>
2022-12-02 15:59:52 +0900

    feat: DeleteInputButton에 Props 추가

--- src/components/Buttons/DeleteInput.tsx
+++ src/components/Buttons/DeleteInput.tsx
@@ -1,19 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { colors } from "assets";
 import { useToggleButton } from "hooks";
-import { RefObject, useCallback } from "react";
-import styled from "styled-components";
+import { ReactNode, RefObject, useCallback } from "react";
+import styled, { css } from "styled-components";
 import { ReactComponent as IcDelete } from "../../assets/images/ic_delete.svg";
 
 interface DeleteInputProps {
   targetRef: RefObject<HTMLInputElement | HTMLTextAreaElement>;
   isShown?: boolean;
+  icon?: ReactNode;
+  className?: string;
   onDelete?(): void;
+  position?: { top: string; left: string } | { bottom: string; right: string };
 }
 export const DeleteInput = ({
   targetRef,
+  icon,
   isShown = true,
   onDelete,
+  position,
 }: DeleteInputProps) => {
   const { hideButton } = useToggleButton();
 
@@ -26,20 +31,37 @@ export const DeleteInput = ({
   }, [targetRef, onDelete]);
 
   return (
-    <DeleteButton onClick={handleClick} disabled={!isShown}>
-      {isShown && <IconDelete />}
+    <DeleteButton onClick={handleClick} disabled={!isShown} position={position}>
+      {isShown && (icon || <DefaultIcon />)}
     </DeleteButton>
   );
 };
 
-const DeleteButton = styled.button`
-  position: absolute;
+const DeleteButton = styled.button<Pick<DeleteInputProps, "position">>`
+  position: relative;
   top: 50%;
-  right: 18px;
   transform: translate(0, -50%);
+
+  ${({ position }) => {
+    if (!position) return;
+    if ("top" in position) {
+      return css`
+        position: absolute;
+        top: ${position.top};
+        left: ${position.left};
+      `;
+    }
+    if ("bottom" in position) {
+      return css`
+        position: absolute;
+        bottom: ${position.bottom};
+        right: ${position.right};
+      `;
+    }
+  }}
 `;
 
-const IconDelete = styled(IcDelete)`
+const DefaultIcon = styled(IcDelete)`
   & > path {
     fill: ${colors("gray35")};
     transition: 400ms all;
